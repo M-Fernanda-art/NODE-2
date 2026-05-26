1dc4ad9b3d7f2746aee39ca3b82eb363e6593c30
M-Fernanda-art <fernanda201298@gmail.com>
2026-05-26 14:51:27 -0500

    ejemplo post done

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-// GET
+// PETICIONES HTTP
+
+// Permiten que frontend y backend se comuniquen para enviar y recibir información.
+// Son mensajes que envía un cliente (como un navegador o una app) a un servidor para pedir o enviar información.
+
+// Cliente: Navegador, aplicación, frontend
+// Servidor: Node.js, Express, backend
+
+// Los métodos HTTP indican QUÉ acción quiere hacer el cliente con la información del servidor.
+
+// Sirven para: iniciar sesión, registrarse, mostrar productos, enviar formularios, guardar datos, obtener información de bases de bases de datos, APIs
+
+
+
+// TIPOS DE PETICIONES
+
+// GET: Sirve para obtener información. Adjunto imagen
 
 const express = require("express");
 
-const appServidor = express();
+const app = express();
+
+app.use(express.json());
 
 // RUTA GET
 
-appServidor.get("/videojuegos", (req, res) => {
+app.get("/videojuegos", (req, res) => {
 
     const listaVideojuegos = [
         {
@@ -28,7 +46,69 @@ appServidor.get("/videojuegos", (req, res) => {
 
 // SERVIDOR
 
-appServidor.listen(3000, () => {
+app.listen(3000, () => {
 
     console.log("Servidor activo en http://localhost:3000");
 });
+
+
+
+// POST: Sirve para enviar o guardar información. Adjunto imagen
+
+const comentarios = [];
+
+app.post("/comentarios", (req, res) => {
+
+    const nuevoComentario = {
+
+        mensaje: req.body.mensaje
+
+    };
+
+comentarios.push(nuevoComentario);
+
+res.json({
+
+    mensaje: "Comentario guardado",
+
+    datos: nuevoComentario
+
+});
+
+});
+
+// GET COMENTARIOS
+
+app.get("/comentarios", (req, res) => {
+
+    res.json(comentarios);
+
+});
+
+// PRINCIPAL
+
+app.get("/", (req, res) => {
+
+    res.send("Servidor funcionando");
+
+});
+
+
+
+// PUT: Sirve para actualizar información completa.
+
+
+
+
+
+
+
+// PATCH: Sirve para actualizar SOLO una parte
+// DELETE: Sirve para eliminar información
+// OPTIONS: Sirve para preguntar qué métodos permite el servidor
+// HEAD: Sirve para obtener información de respuesta SIN contenido.
+// CONNECT: Sirve para crear conexiones seguras
+// TRACE: Sirve para diagnóstico y depuración.
+
+
+
